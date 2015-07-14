68352ea9136bd542f29d6994251c67dca8510004
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2015-07-14 12:41:58 +0200

    Initial tests

---
+++ test.js
@@ -0,0 +1,32 @@
+
+function wrapper(){
+    
+    /** descrizione */
+    function namefs(paramA){
+        this.prop = 'a';
+    }
+
+    /**
+     * [addL description]
+     */
+    namefs.prototype.addL = function(){
+
+    }
+
+    /** Contructor */
+    var Car = function(){
+        /** NO */
+        this.innerProp  = function(){}
+    }
+
+    Car.prototype.accelerate = function(){}
+    Car.prototype.accelerate = function(){}
+    Car.prototype.accelerate = function(){}
+    Car.prototype.accelerate = function(){}
+    Car.prototype.accelerate = function(){}
+
+    /**NEANCHETU!*/
+    var object = {
+
+    }
+}
